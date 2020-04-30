7fc2465e4368261d20def1d84ab86eb3d7fc4828
Leandry <leandry@atwix.com>
2020-04-30 18:24:42 +0200

    Calculate avarage in page load

--- page_load_checker.bash
+++ page_load_checker.bash
@@ -18,11 +18,6 @@ close_on_error(){
         exit 99
 }
 
-#check_variables(){
-#    [[ -z ${URL} ]] && close_on_error "--environment argument not set" && display_help
-#    [[ -z ${ITERATIONS} ]] && close_on_error "--project argument not set" && display_help
-#}
-
 validate_url(){
     regex='(https?|http?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
     [[ -z ${URL} ]] && read -p "Enter website URL:  " URL
@@ -49,10 +44,40 @@ validate_iterations(){
 test_url(){
 for ((i=1; i<=${ITERATIONS}; i++))
     do
-        curl --silent -o /dev/null -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" ${URL}
-done
-}
+        PAGE_LOAD_INFO=$(curl --silent -o /dev/null -w "%{time_connect} %{time_starttransfer} %{time_total} \n" ${URL})
 
+        connect_arr[$i-1]=$(echo ${PAGE_LOAD_INFO} | awk '{print $1}')
+        ttfb_arr[$i-1]=$(echo ${PAGE_LOAD_INFO} | awk '{print $2}')
+        total_arr[$i-1]=$(echo ${PAGE_LOAD_INFO} | awk '{print $3}')
+
+        echo "Connect: ${connect_arr[$i-1]} TTFB: ${ttfb_arr[$i-1]} Total Time: ${total_arr[$i-1]}"
+done
+echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+connect_arr_count=0; ttfb_arr_total=0; total_arr_total=0;
+connect_total=0; ttfb_arr_count=0; total_arr_count=0;
+for i in ${connect_arr[@]}
+   do
+     connect_total=$(echo $connect_total+$i | bc )
+     ((connect_arr_count++))
+   done
+for i in ${ttfb_arr[@]}
+   do
+     ttfb_arr_total=$(echo $ttfb_arr_total+$i | bc )
+     ((ttfb_arr_count++))
+   done
+for i in ${total_arr[@]}
+   do
+     total_arr_total=$(echo $total_arr_total+$i | bc )
+     ((total_arr_count++))
+   done
+avarage_connect=$(echo "scale=2; $connect_total / $connect_arr_count" | bc)
+avarage_ttfb=$(echo "scale=2; $ttfb_arr_total / $ttfb_arr_count" | bc)
+avarage_total=$(echo "scale=2; $total_arr_total / $total_arr_count" | bc)
+echo "Average Connect Time: ${avarage_connect}"
+echo "Average TTFB Time: ${avarage_ttfb}"
+echo "Average Total Time: ${avarage_total}"
+
+}
 while [[ $# -ne 0 ]]
 do
     arg="$1"
